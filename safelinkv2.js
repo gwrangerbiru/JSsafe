320fa0bafaaaa7bb78d452e0639a3e88a25df0a5
Rizky <b6836ksf@gmail.com>
2017-07-26 21:20:17 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="4-mistakes-most-reserved-all-risk.html"}
 if(x=="2"){xxx="you-should-know-not-all-of-risk-borne.html"}
 if(x=="3"){xxx="terms-in-car-insurance.html"}
-if(x=="4"){xxx="whether-all-risks-already-covered-by.html"}a_to_ve[i].href="http://mygameapksavelink.blogspot.com/2017/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="whether-all-risks-already-covered-by.html"}a_to_ve[i].href="https://mygameapksavelink.blogspot.com/2017/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
